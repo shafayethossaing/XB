9ad9fcc8e4609e2c067bfca5339594d0e2acc27b
shafayethossaing <shafayethossaing1@gmail.com>
2024-04-17 10:26:06 +0600

    week 4, day-7

---
+++ Week-4/Day-7/Xorry_2.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define ll long long int
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define vi vector<int>
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+#define sorta(arr) sort(arr.begin(), arr.end());
+#define sortd(arr) sort(arr.begin(), arr.end(), greater<int>());
+#define All(X) (X).begin(),(X).end()
+#define Unique(X) (X).erase(unique((X).begin(),(X).end()),(X).end())
+#define range(arr) for(auto el: arr) cout<<el<<" ";
+#define Rocket ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main(){ Rocket;
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int i = 31;
+        int setBit = 0;
+        while(i >= 0){
+            if(n & (1 << i)){
+                setBit++;
+                if(setBit == 2) break;
+            }
+            i--;
+        }   
+
+        int cnt = 0;
+        while(i >= 0){
+            if((n & (1 << i)) == 0){
+                cnt++;
+            }
+            i--;
+        }  
+        cout<<(1 << cnt)<<endl;
+    }
+    return 0;
+}
